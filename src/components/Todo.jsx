568b16c0bc688593c7f9ef88ddb35cba89cdfa65
jules <naranjojulian702@gmail.com>
2023-04-24 14:29:47 -0700

    all logic and gui code seperated on Todo.jsx. Next ill format the code and make it cleaner with sections.

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { db, auth } from './Firebase'
-import { get, set, ref, child, onValue, orderByChild, query, remove, update} from 'firebase/database'; 
+import { get, set, ref, child, onValue, orderByChild, query, remove, update, orderByValue} from 'firebase/database'; 
 import { motion } from 'framer-motion'
 import Menu from './Menu';
 import TodoItem from './TodoItem';
@@ -9,21 +9,16 @@ import '../sass/main.css'
 const Todo = () => {
   let [menu, setMenu] = useState(false); 
   let [menuIcon, setMenuIcon] =useState(true); 
-
-  //? grab something from local storage 
-
   let selTodo = localStorage.getItem('selected_todo'); 
 
   let [todoMenu, setTodoMenu] = useState(false); 
-  let [selectedTodo, setSelectedTodo] = useState(''); 
 
-  // let uid = localStorage.getItem('user_uid'); 
   
   let pullTodos = () => {
     auth.onAuthStateChanged(cred => {
       let uid = cred.uid
       console.log(uid); 
-      let dbRef = query(ref(db, `${uid}/todos/`), orderByChild('createdAt'))
+      let dbRef = query(ref(db, `${uid}/todos/`), orderByChild('created_At'))
       onValue(dbRef, todo_item => {
         let todoCont = document.querySelector('.todoCont'); 
         todoCont.innerHTML = ''; 
@@ -31,8 +26,11 @@ const Todo = () => {
           let todoText = todoNode.val().todoText
           let todoItem = document.createElement('h1'); 
           todoItem.classList.add('todoItem'); 
-          todoItem.classList.add('bg-blue-900')
+          todoItem.classList.add('bg-white')
+          todoItem.classList.add('shadow-lg')
+          todoItem.classList.add('text-blue-500')
           todoItem.classList.add('hover:cursor-pointer')
+          todoItem.classList.add('rounded-xl')
           todoItem.textContent = todoText; 
           todoCont.appendChild(todoItem); 
           todoItem.addEventListener('click', (e) => {
@@ -55,10 +53,9 @@ const Todo = () => {
       let uid = cred.uid
       let todoInput = document.querySelector('.todoInput'); 
       let todoInputText = todoInput.value; 
-      // let ls_uid = localStorage.getItem('user_uid'); 
       set(ref(db, `${uid}/todos/${todoInputText}`), {
         todoText: todoInputText, 
-        createdAt: Date()
+        created_At: Date()
       })
       todoInput.value = ''; 
     })
@@ -66,24 +63,34 @@ const Todo = () => {
 
   let updateTodo = (e) => {
     auth.onAuthStateChanged(cred => {
-      let uid = cred.uid
-      let todoInput = document.querySelector('.todoInput'); 
-      let todoInputText = todoInput.value; 
-      set(ref(db, `${uid}/todos/${todoInputText}`), {
-        todoText: todoInputText, 
-        createdAt: Date()
+      let uid = cred.uid; 
+      let selTodo = localStorage.getItem('selected_todo'); 
+      let updateTodoText = document.querySelector(".updateTodoText")
+      let updatedTodo = updateTodoText.textContent; 
+      console.log(selTodo); 
+      remove(ref(db, `${uid}/todos/${selTodo}`))
+      set(ref(db, `${uid}/todos/${updatedTodo}`), {
+        todoText: updatedTodo, 
+        created_At: Date()
       })
+      setTodoMenu(false); 
     })
   }
 
   let removeTodo = () => {
-    let selTodo = localStorage.getItem('selected_todo'); 
     auth.onAuthStateChanged(cred => {
-      remove(ref(db, `${uid}/todos/${selTodo}`))
-      .catch((err) => {
-        console.log(err.message); 
-      })
+      let uid = cred.uid
+      let removeTodo = localStorage.getItem('selected_todo'); 
+      remove(ref(db, `${uid}/todos/${removeTodo}`))
+      setTodoMenu(false); 
     })
+  }
+
+  let handleEnterKey = (e) => {
+    let todoInput = document.getElementById('todoInput'); 
+              if (e.keyCode === 13) {
+                let newTodo = document.getElementById('newTodo').click(); 
+              }
   }
 
   pullTodos()
@@ -101,11 +108,11 @@ const Todo = () => {
         </div>
 
         <div className='h-[95%] w-full bg-gray-700 flex flex-col'>
-          <div className='inputCont h-15% w-full flex justify-center items-center '>
-            <input className='todoInput p-2 h-[100%] w-[90%] bg-gray-800 text-white ' type='text' placeholder='What Is There 2Due Today?' />
-            <button className='p-2 bg-blue-700 h-[100%] w-[10%] text-white' onClick={writeTodos}>New 2Due</button>
+          <div className='inputCont h-15% w-full flex justify-center items-center mb-2 '>
+            <input id='todoInput' className='todoInput p-2 h-[100%] w-[90%] bg-gray-800 text-white z-50 ' maxLength='140' type='text' placeholder='What Is There 2Due Today?' onKeyDown={handleEnterKey}/>
+            <button id='newTodo' className='p-2 bg-blue-700 h-[100%] w-[10%] text-white' onClick={writeTodos}>New 2Due</button>
           </div>
-          <div className='todoCont h-[85%] w-full flex flex-col gap-2 '>
+          <div className='todoCont h-[100%] w-full flex flex-col gap-4 items-center overflow-y-scroll'>
 
           </div>
 
@@ -114,28 +121,8 @@ const Todo = () => {
           todoMenu ? <motion.div initial={{y: '-100vh'}} animate={{y: 0}} className='h-screen w-full bg-gray-900 absolute z-20 opacity-[90%] flex flex-col gap-5 justify-center items-center'>
             <h1 className='text-white font-bold text-xl'>Edit 2DUE:</h1>
             <p className='updateTodoText text-white text-4xl font-bold' contentEditable='true'>{selTodo}</p>
-            <i className="fa-solid fa-trash-can text-red-500 text-3xl hover:cursor-pointer" onClick={() => {
-              auth.onAuthStateChanged(cred => {
-                let uid = cred.uid
-                let removeTodo = localStorage.getItem('selected_todo'); 
-                remove(ref(db, `${uid}/todos/${removeTodo}`))
-                setTodoMenu(false); 
-              })
-            }}></i>
-            <i className="fa-solid fa-pen-to-square text-3xl hover:cursor-pointer text-white" onClick={() => {
-              auth.onAuthStateChanged(cred => {
-                let uid = cred.uid; 
-                let selTodo = localStorage.getItem('selected_todo'); 
-                let updateTodoText = document.querySelector(".updateTodoText")
-                let updatedTodo = updateTodoText.textContent; 
-                console.log(selTodo); 
-                remove(ref(db, `${uid}/todos/${selTodo}`))
-                set(ref(db, `${uid}/todos/${updatedTodo}`), {
-                  todoText: updatedTodo
-                })
-                setTodoMenu(false); 
-              })
-            }}></i>
+            <i className="fa-solid fa-trash-can text-red-500 text-3xl hover:cursor-pointer" onClick={removeTodo}></i>
+            <i className="fa-solid fa-pen-to-square text-3xl hover:cursor-pointer text-white" onClick={updateTodo}></i>
             <button className='text-white text-xl p-2 bg-blue-900' onClick={() => { setTodoMenu(false) }}>Close</button>
           </motion.div> : null
         }
