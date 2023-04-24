77a3653ec437b05271a7ee2c4b6b5ae714bc3840
jules <naranjojulian702@gmail.com>
2023-04-24 16:15:15 -0700

    added a better format for the time and date the 2Due is posted. bypassed the timestamp text from being selected for edit or remove

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -21,29 +21,33 @@ const Todo = () => {
   let pullTodos = () => {
     auth.onAuthStateChanged(cred => {
       let uid = cred.uid
-      console.log(uid); 
       let dbRef = query(ref(db, `${uid}/todos/`), orderByChild('created_At'))
       onValue(dbRef, todo_item => {
         let todoCont = document.querySelector('.todoCont'); 
         todoCont.innerHTML = ''; 
         todo_item.forEach(todoNode => {
           let todoText = todoNode.val().todoText
+          let todoDiv = document.createElement('div'); 
           let todoItem = document.createElement('h1'); 
           todoItem.classList.add('todoItem'); 
-          todoItem.classList.add('bg-white')
+          todoItem.classList.add('bg-blue-400')
           todoItem.classList.add('shadow-lg')
-          todoItem.classList.add('text-blue-500')
+          todoItem.classList.add('text-white')
           todoItem.classList.add('hover:cursor-pointer')
           todoItem.classList.add('rounded-xl')
+          todoItem.classList.add('z-30'); 
           todoItem.textContent = todoText; 
+          let timeStamp = document.createElement('h1'); 
+          timeStamp.textContent = todoNode.val().created_At; 
+          timeStamp.classList.add('text-sm')
+          timeStamp.classList.add('pointer-events-none')
           todoCont.appendChild(todoItem); 
+          todoItem.append(timeStamp)
           todoItem.addEventListener('click', (e) => {
+            console.log(e.target.firstChild.textContent); 
             setTodoMenu(todoMenu => !todoMenu)
-            let selectedTodo = e.target.textContent; 
+            let selectedTodo = e.target.firstChild.textContent; 
             let lsTodo = localStorage.setItem('selected_todo', `${selectedTodo}`)
-            let testTodo = localStorage.getItem('selected_todo'); 
-            console.log(testTodo + "is the test from local storage"); 
-            console.log(selectedTodo + 'is the dom selected todo')
             setTodoMenu(true); 
           })
         }) 
@@ -59,12 +63,20 @@ const Todo = () => {
       let uid = cred.uid
       let todoInput = document.querySelector('.todoInput'); 
       let todoInputText = todoInput.value; 
+      let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+      let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+      let hours = ['(AM)12', '(AM)1', '(AM)2', '(AM)3', '(AM)4', '(AM)5', '(AM)6', '(AM)7','(AM)8', '(AM)9', '(AM)10', '(AM)11', '(PM)12', '(PM)1','(PM)2', '(PM)3', '(PM)4','(PM)5', '(PM)6', '(PM)7','(PM)8', '(PM)9', '(PM)10','(PM)11' ]
+      const d = new Date(); 
+      let hour = hours[d.getHours()]
+      let minutes = d.getMinutes(); 
+      let day = days[d.getDay()]; 
+      let month = months[d.getMonth()]
       if (todoInputText === '') {
-        alert('You must use the input field to write a todo!')
+        alert('You must use the input field to write a 2Due!')
       } else {
         set(ref(db, `${uid}/todos/${todoInputText}`), {
           todoText: todoInputText, 
-          created_At: Date()
+          created_At: `${hour}:${d.getMinutes()}:${d.getSeconds()}s ${day} ${month} ${d.getFullYear()}`
         })
         todoInput.value = ''; 
       }
@@ -77,11 +89,18 @@ const Todo = () => {
       let selTodo = localStorage.getItem('selected_todo'); 
       let updateTodoText = document.querySelector(".updateTodoText")
       let updatedTodo = updateTodoText.textContent; 
-      console.log(selTodo); 
+      let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+      let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+      let hours = ['(AM)12', '(AM)1', '(AM)2', '(AM)3', '(AM)4', '(AM)5', '(AM)6', '(AM)7','(AM)8', '(AM)9', '(AM)10', '(AM)11', '(PM)12', '(PM)1','(PM)2', '(PM)3', '(PM)4','(PM)5', '(PM)6', '(PM)7','(PM)8', '(PM)9', '(PM)10','(PM)11' ]
+      const d = new Date(); 
+      let hour = hours[d.getHours()]
+      let minutes = d.getMinutes(); 
+      let day = days[d.getDay()]; 
+      let month = months[d.getMonth()]
       remove(ref(db, `${uid}/todos/${selTodo}`))
       set(ref(db, `${uid}/todos/${updatedTodo}`), {
         todoText: updatedTodo, 
-        created_At: Date()
+        created_At: `${hour}:${d.getMinutes()}:${d.getSeconds()}s ${day} ${month} ${d.getFullYear()}`
       })
       setTodoMenu(false); 
     })
@@ -123,7 +142,7 @@ const Todo = () => {
           }
         </div>
 
-        <div className='h-[95%] w-full bg-gray-700 flex flex-col'>
+        <div className='h-[95%] w-full bg-gradient-to-b from-indigo-200 to-blue-300 flex flex-col '>
           <div className='inputCont h-15% w-full flex justify-center items-center mb-2 '>
             <input id='todoInput' className='todoInput p-2 h-[100%] w-[90%] bg-gray-800 text-white z-50 ' maxLength='140' type='text' placeholder='What Is There 2Due Today?' onKeyDown={handleEnterKey}/>
             <button id='newTodo' className='p-2 bg-blue-700 h-[100%] w-[10%] text-white' onClick={writeTodos}>New 2Due</button>
@@ -131,10 +150,14 @@ const Todo = () => {
           <div className='todoCont h-[100%] w-full flex flex-col gap-4 items-center overflow-y-scroll'>
             {/* todos go into here ordered by time of creation! */}
           </div>
-
+          
+        <div className='sectionNav flex gap-10 justify-center mb-5 flex-wrap'>
+            <button className='privCollections p-2 bg-blue-500 rounded-md text-white font-bold'>Private 2Dues!</button>
+            <button className='pubCollections p-2 bg-blue-500 rounded-md text-white font-bold'>Shared 2Dues!</button>
+          </div>
         </div>
         {
-          todoMenu ? <motion.div initial={{y: '-100vh'}} animate={{y: 0}} className='h-screen w-full bg-gray-900 absolute z-20 opacity-[90%] flex flex-col gap-5 justify-center items-center'>
+          todoMenu ? <motion.div initial={{y: '-100vh'}} animate={{y: 0}} className='h-screen w-full bg-gray-900 absolute z-[100] opacity-[92%] flex flex-col gap-5 justify-center items-center'>
             <h1 className='text-white font-bold text-xl'>Edit 2DUE:</h1>
             <p className='updateTodoText text-white text-4xl font-bold' contentEditable='true'>{selTodo}</p>
             <i className="fa-solid fa-trash-can text-red-500 text-3xl hover:cursor-pointer" onClick={removeTodo}></i>
@@ -142,6 +165,7 @@ const Todo = () => {
             <button className='text-white text-xl p-2 bg-blue-900' onClick={() => { setTodoMenu(false) }}>Close</button>
           </motion.div> : null
         }
+
     </div>
   )
 }
