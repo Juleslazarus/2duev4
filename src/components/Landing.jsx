c6a80e2bc529fb86cdcfc6363c2e1bab1739f6b2
jules <naranjojulian702@gmail.com>
2023-04-28 15:16:08 -0700

    tweaking animations to raise lighthouse scores

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -16,12 +16,12 @@ const Landing = () => {
 
   return (
     <div className='landingComp h-[100vh] w-full bg-gray-900 flex flex-col gap-8 justify-center items-center'>
-        <motion.h1 initial={{y: -20, opacity: 0, scale: .9}} animate={{y: 0, opacity: 1, scale: 1 }} transition={{type: 'tween', duration: 1}} className='text-white text-3xl font-bold text-center'>Welcome To 2Due v4</motion.h1>
+        <motion.h1 initial={{y: -20, opacity: 0, scale: .9}} animate={{y: 0, opacity: 1, scale: 1 }} transition={{type: 'tween', duration: .2}} className='text-white text-3xl font-bold text-center'>Welcome To 2Due v4</motion.h1>
         <div className='choicesCont flex gap-8 flex-wrap justify-center items-center'>
-            <motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{type: 'tween', duration: 1, delay: .5}} className='h-[8rem] w-[8rem] border-solid border-blue-900 border-2 flex justify-center items-center transition-all ease-in-out hover:cursor-pointer hover:scale-[1.2]' onClick={() => {setLog(log => !log); setReg(false)}}>
+            <motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{type: 'tween', duration: .2, delay: .5}} className='h-[8rem] w-[8rem] border-solid border-blue-900 border-2 flex justify-center items-center transition-all ease-in-out hover:cursor-pointer hover:scale-[1.2]' onClick={() => {setLog(log => !log); setReg(false)}}>
                 <h1 className='text-white text-1xl font-bold'>Log In</h1>
             </motion.div>
-            <motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{type: 'tween', duration: 1, delay: .5}} className='h-[8rem] w-[8rem] border-solid border-blue-900 border-2 flex justify-center items-center transition-all ease-in-out hover:cursor-pointer hover:scale-[1.2]' onClick={() => {setReg(reg => !reg); setLog(false) }}>
+            <motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{type: 'tween', duration: .2, delay: .5}} className='h-[8rem] w-[8rem] border-solid border-blue-900 border-2 flex justify-center items-center transition-all ease-in-out hover:cursor-pointer hover:scale-[1.2]' onClick={() => {setReg(reg => !reg); setLog(false) }}>
                 <h1 className='text-white text-1xl font-bold'>Register</h1>
             </motion.div>
         </div>
